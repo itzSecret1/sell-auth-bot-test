954a5d43c22a7c6445c4eecd908a4e114c211d3a
itzSecret1 <falso0518@gmail.com>
2025-11-22 12:58:23 +0100

    Add utils/config.js

---
+++ utils/config.js
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+
+let config = {
+  BOT_TOKEN: process.env.BOT_TOKEN || '',
+  BOT_GUILD_ID: process.env.BOT_GUILD_ID || '',
+  BOT_USER_ID_WHITELIST: process.env.BOT_USER_ID_WHITELIST?.split(',') || [],
+  BOT_CUSTOMER_ROLE_ID: process.env.BOT_CUSTOMER_ROLE_ID || '',
+  BOT_STAFF_ROLE_ID: process.env.BOT_STAFF_ROLE_ID || '',
+  BOT_ADMIN_ROLE_ID: process.env.BOT_ADMIN_ROLE_ID || '',
+  SA_API_KEY: process.env.SA_API_KEY || '',
+  SA_SHOP_ID: process.env.SA_SHOP_ID || ''
+};
+
+export { config };
